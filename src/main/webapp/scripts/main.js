6137bc2c6365ab83c1d5630241dc1f2bf1c6c408
969084 <969084@D4021L59.corp.ds.fedex.com>
2016-04-27 11:38:55 -0400

    [v1] commit base project with simple ajs template

---
+++ src/main/webapp/scripts/main.js
@@ -0,0 +1,30 @@
+require.config({
+	  map:{
+
+	  },
+	  paths:{
+		  'angular': '../js/angular-1.4.5/angular',
+		  'app': 'app',
+		  'coreModule': 'coreModule',
+		  'jquery': '../js/jquery/jquery-2.2.3'
+	  },
+	  shim:{
+		  'app': {
+			  deps: ['angular', 'jquery', 'coreModule']
+		  },
+		  'coreModule': {
+			  deps: ['angular']
+		  }
+	  }
+});
+
+require(['app'], function(){
+	angular.bootstrap(document, ['app']);
+});
+
+define(['jquery'], function($) {
+
+	$('.message a').click(function() {
+		$('form').animate({height : "toggle", opacity : "toggle"}, "slow");
+	});
+});
